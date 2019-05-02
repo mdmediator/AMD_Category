e5defec5e4b8ad08df6acc92b64eda7c6c377745
wuzj <404899071@qq.com>
2019-05-02 22:21:03 +0800

    Initial Commit

---
+++ AMD_CategoryTests/AMD_CategoryTests.m
@@ -0,0 +1,37 @@
+//
+//  AMD_CategoryTests.m
+//  AMD_CategoryTests
+//
+//  Created by jer on 2019/5/2.
+//  Copyright © 2019 jer. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AMD_CategoryTests : XCTestCase
+
+@end
+
+@implementation AMD_CategoryTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
